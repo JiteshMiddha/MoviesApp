cfc44a6bc43b4ddd11d20034d8f47b27e94cc4fc
Jitesh <jiteshmiddha06@gmail.com>
2021-05-29 14:17:25 +0530

    Initial Commit

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Jitesh Middha on 29/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
